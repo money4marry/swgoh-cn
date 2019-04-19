998488fdaa2b92ef524acfc57db15891982217bb
王阳 <wangyang@klicen.com>
2019-04-19 17:46:06 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,76 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <!--<el-input v-model="input" placeholder="请输入内容"></el-input>-->
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import request from '@/utils/request';
+    import Cookies from 'js-cookie'
+    // import Api from './utils/api-swgoh-help';
+    // import example from './utils/example.js';
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'app',
+        data() {
+            return {
+                api: null,
+                allycode: '263953972'
+            };
+        },
+        components: {},
+        created() {
+            // example();
+            // this.login();
+            this.getPlayers();
+            // const api = new Api({
+            //     username: 'frankwy',
+            //     password: 'hello520wanan'
+            // })
+            // api.connect().then((res) => {
+            //     console.log(res)
+            // });
+        },
+        methods: {
+            login() {
+                const params = {
+                    username: 'frankwy',
+                    password: 'hello520wanan',
+                    grant_type: 'password',
+                    client_id: '123',
+                    client_secret: 'abc'
+                };
+                const data = `username=${params.username}&password=${params.password}&grant_type=${params.grant_type}&client_id=${params.client_id}&client_secret=${params.client_secret}`;
+                request({
+                    url: '/auth/signin',
+                    method: 'post',
+                    data
+                }).then((res) => {
+                    // Cookies.set('token_type', res.token_type);
+                    Cookies.set('access_token', res.access_token);
+                    console.log('登陆成功');
+                });
+            },
+            getPlayers() {
+                request({
+                    url: '/swgoh/players',
+                    method: 'POST',
+                    data: {
+                        allycodes: [this.allycode],
+                        allycode: this.allycode
+                    }
+                }).then((res) => {
+                    console.log(res)
+                });
+            }
+        }
+    };
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+    }
 </style>
---
+++ src/utils/request.js
@@ -0,0 +1,35 @@
+import axios from 'axios'
+import { getToken } from '@/utils/auth'
+
+// 创建axios实例
+const service = axios.create({
+  baseURL: 'https://api.swgoh.help', // api 的 base_url
+  timeout: 15000 // 请求超时时间
+})
+
+// request拦截器
+service.interceptors.request.use(
+  config => {
+    if (getToken()) {
+      config.headers['Authorization'] = 'Bearer ' + getToken() // 让每个请求携带自定义token 请根据实际情况自行修改
+    }
+    config.headers['Content-Type'] = 'application/json'
+    return config
+  },
+  error => {
+    // Do something with request error
+    console.log(error) // for debug
+    Promise.reject(error)
+  }
+)
+
+// response 拦截器
+service.interceptors.response.use(
+  response => {
+    return response.data
+  },
+  error => {
+    return Promise.reject(error)
+  }
+)
+export default service
